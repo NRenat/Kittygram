ecc12066f2d350b15dec7de9fe6461ecb85cf756
NRenat <nrenat1606@yandex.ru>
2023-11-29 08:01:25 +0300

    fix gitactions tests

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'none').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
